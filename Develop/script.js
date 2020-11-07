730d8100c2316d8c86a3de62b0d5941558ab0ed9
Jessica D <deck.jessica@gmail.com>
2020-11-06 21:25:24 -0500

    cleaned up generate password func, renamed length to pwLength, confirm for each cleaned up

--- Develop/script.js
+++ Develop/script.js
@@ -14,6 +14,25 @@ var specialCase = ['~','!','@','#','$','%','^','&','*','(',')','_','-','+','=','
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//function to generate password based on returned criteria
+function generatePW(){
+		var pwLength = parseInt(prompt("Enter the length of your password between 8 and 128 characters:"));
+		if (!pwLength) {
+		  alert("this needs a value");
+		  return;
+		} if ( pwLength < 8 || pwLength > 128 ) {
+		  pwLength = parseInt(prompt("Choose a number between 8 and 128"));
+			return;
+		}  
+
+		let confirmNumber = confirm("Would you like to include numbers?");
+		let confirmCharacter = confirm("Would you like to include special characters?");
+		let confirmUpper = confirm("Would you like to include uppercase letters?");
+		let confirmLower = confirm("Would you like to include lowercase letters?");
+		
+		
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -24,20 +43,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword() {
-    var length = parseInt(prompt("Enter the length of your password between 8 and 128 characters:"));
-    if (!length) {
-      alert("this needs a value");
-    } else if ( length < 8 || length > 128 ) {
-      length = parseInt(prompt("Choose a number between 8 and 128"));
-    }  else {
-      confirmNumber = confirm("Would you like to include numbers?");
-      confirmCharacter = confirm("Would you like to include special characters?");
-      confirmUpper = confirm("Would you like to include uppercase letters?");
-      confirmLower = confirm("Would you like to include lowercase letters?");
-    }
-    password.innerText = generatePassword(confirmNumber, confirmCharacter, confirmUpper, confirmLower, length);
-});
+generateBtn.addEventListener("click", writePassword); 
 
 
 
